0226fa0fe50006c804b59af1e3f8703742a59de9
abudabi7338 <114689374+abudabi7338@users.noreply.github.com>
2023-07-13 21:00:35 +0200

    Add JS function
    Add js function for button complete

--- js/script.js
+++ js/script.js
@@ -3,25 +3,29 @@ const btnAdd = document.querySelector('.btn-add');
 const downContainer = document.querySelector('.down-container');
 const noTask = document.querySelector('.no-task');
 
-let newLi
+let newLi;
+let btnCorrect;
 
 const ulList = document.querySelector('.task');
 
 const createToolsArea = () => {
-	 const toolsPanel = document.createElement('div');
+	const toolsPanel = document.createElement('div');
 	toolsPanel.classList.add('tools-area');
 
-	const btnCorrect = document.createElement('button');
+	btnCorrect = document.createElement('button');
 	btnCorrect.innerHTML = '<i class="fa-solid fa-check" style="color: #05c736;"></i>';
+	btnCorrect.classList.add('complete');
 
 	const btnDelete = document.createElement('button');
 	btnDelete.innerHTML = '<i class="fa-solid fa-x" style="color: #e10e0e;"></i>';
+	btnDelete.classList.add('delete');
 
 	const btnEdit = document.createElement('button');
 	btnEdit.innerHTML = '<i class="fa-solid fa-pen-to-square"></i>';
+	btnEdit.classList.add('edit');
 
 	toolsPanel.append(btnCorrect, btnDelete, btnEdit);
-	newLi.append(toolsPanel)
+	newLi.append(toolsPanel);
 };
 
 const createTask = () => {
@@ -29,9 +33,21 @@ const createTask = () => {
 	newLi = document.createElement('li');
 	newLi.textContent = `${inputValue}`;
 	ulList.append(newLi);
-	
+	noTask.remove();
 
 	createToolsArea();
 };
 
+const checkClick = e => {
+	if (e.target.matches('.complete')) {
+		e.target.closest('li').classList.toggle('completed');
+	} else if (e.target.matches('.delete')) {
+		console.log('delete');
+	} else if (e.target.matches('.edit')) {
+		console.log('edit');
+	}
+};
+
 btnAdd.addEventListener('click', createTask);
+
+ulList.addEventListener('click', checkClick);
